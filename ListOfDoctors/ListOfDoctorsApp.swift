5e6e2cd41a076c52188541625e1594e0f75faf01
nik97111 <70099213+nik97111@users.noreply.github.com>
2024-03-11 19:19:50 +0300

    Initial Commit

---
+++ ListOfDoctors/ListOfDoctorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfDoctorsApp.swift
+//  ListOfDoctors
+//
+//  Created by Николай Чупреев on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ListOfDoctorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
